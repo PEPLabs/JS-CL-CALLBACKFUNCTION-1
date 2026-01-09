83b7f1c5bc1c599cf8d7cc79ce6a3798b6cc6836
Tanisi90 <69205416+Tanisi90@users.noreply.github.com>
2026-01-09 07:22:27 -0700

    Update SeleniumTest.java
    updated path to absolute path.

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -5,45 +5,40 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
-import java.time.Duration;
 
 public class SeleniumTest {
     private WebDriver webDriver;
 
     @Before
     public void setUp() {
-        
-        System.setProperty("webdriver.edge.driver", "driver/msedgedriver");//linux_64
 
-        // Get file
+        // Keep your exact string, but normalize it (portable fix)
+        File chromeDriver = new File("driver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", chromeDriver.getAbsolutePath());
+
         File file = new File("index.html");
         String path = "file://" + file.getAbsolutePath();
 
-        
-        EdgeOptions options = new EdgeOptions();
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
-        webDriver = new EdgeDriver(options);
+        webDriver = new ChromeDriver(options);
 
-        // Open the HTML file
         webDriver.get(path);
     }
 
     @After
     public void tearDown() {
-        // Close the browser
-        webDriver.quit();
+        if (webDriver != null) {
+            webDriver.quit();
+        }
     }
 
     @Test
     public void testPageText() throws InterruptedException {
-        //setup
         WebElement speechPart1 = webDriver.findElement(By.id("opening"));
         String opening = "To be, or not to be, that is the question:";
         WebElement speechPart2 = webDriver.findElement(By.id("theRest"));
@@ -54,6 +49,4 @@ public class SeleniumTest {
         Assert.assertEquals(opening, speechPart1.getText());
         Assert.assertEquals(theRest, speechPart2.getText().substring(0, 41));
     }
-
-
 }
